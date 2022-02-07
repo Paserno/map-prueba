177721a61974b80a5ed1681b662e6001e1be3967
BryanG1995 <bryan.gutierrez1995@gmail.com>
2022-02-06 21:43:00 -0300

    cambios pequenos

--- src/map/map.js
+++ src/map/map.js
@@ -1,7 +1,16 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiMjVwYXNzZXIiLCJhIjoiY2t4MHNtNGh0MTB3dDJubHlrcHF2dnpmMiJ9.YQrtunF-GEy5DtzbsFweww';
+
+const long = -71.5209854;
+const lat = -33.0214519;
+
+
         const map = new mapboxgl.Map({
             container: 'map', // container ID
             style: 'mapbox://styles/mapbox/streets-v11', // style URL
-            center: [-71.5209, -33.0214], // starting position [lng, lat]
-            zoom: 9 // starting zoom
+            center: [long, lat], // starting position [lng, lat]
+            zoom: 18 ,  // starting zoom
+            hash: true
         });
+
+
+        
