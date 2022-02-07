8848d5d0616387695d2f1e8c5c0dceb72a3db65b
BryanG1995 <bryan.gutierrez1995@gmail.com>
2022-02-07 10:12:15 -0300

    puntos en el mapa
    se agregaron punto en el mapa en el cual al hacer click, se logra ir a esa ubicacion

--- src/map/map.js
+++ src/map/map.js
@@ -1,7 +1,7 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiMjVwYXNzZXIiLCJhIjoiY2t4MHNtNGh0MTB3dDJubHlrcHF2dnpmMiJ9.YQrtunF-GEy5DtzbsFweww';
 
 const long = -71.5209854;
-const lat = -33.0214519;
+const lat = -37.0214519;
 
 
         const map = new mapboxgl.Map({
@@ -13,4 +13,78 @@ const lat = -33.0214519;
         });
 
 
-        
+        map.on('load', () => {
+            // Add a GeoJSON source with 3 points.
+            map.addSource('points', {
+            'type': 'geojson',
+            'data': {
+            'type': 'FeatureCollection',
+            'features': [
+            {
+            'type': 'Feature',
+            'properties': {},
+            'geometry': {
+            'type': 'Point',
+            'coordinates': [long, lat]
+            }
+            },
+            {
+            'type': 'Feature',
+            'properties': {},
+            'geometry': {
+            'type': 'Point',
+            'coordinates': [-90.3295, -0.6344]
+            }
+            },
+            {
+            'type': 'Feature',
+            'properties': {},
+            'geometry': {
+            'type': 'Point',
+            'coordinates': [-91.3403, 0.0164]
+            }
+            }
+            ]
+            }
+            });
+
+
+            // Add a circle layer
+            map.addLayer({
+            'id': 'circle',
+            'type': 'circle',
+            'source': 'points',
+            'paint': {
+            'circle-color': '#4264fb',
+            'circle-radius': 8,
+            'circle-stroke-width': 2,
+            'circle-stroke-color': '#ffffff'
+            }
+            });
+             
+            // Center the map on the coordinates of any clicked circle from the 'circle' layer.
+            map.on('click', 'circle', (e) => {
+            map.flyTo({
+            center:  e.features[0].geometry.coordinates,
+            zoom: 17
+            });
+            
+            });
+             
+            // Change the cursor to a pointer when the it enters a feature in the 'circle' layer.
+            map.on('mouseenter', 'circle', () => {
+            map.getCanvas().style.cursor = 'pointer';
+            });
+             
+            // Change it back to a pointer when it leaves.
+            map.on('mouseleave', 'circle', () => {
+            map.getCanvas().style.cursor = '';
+            });
+            });
+
+            map.on('click', 'states-layer', (e) => {
+                new mapboxgl.Popup()
+                .setLngLat(e.lngLat)
+                .setHTML(e.features[0].properties.name)
+                .addTo(map);
+                });
