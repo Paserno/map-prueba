9d4f7d3c4bede917ad7d397a7520050287db4f97
Paserno <paserno.x@gmail.com>
2022-02-08 00:49:59 -0300

    Encapsulamiento de circle layer

--- src/map/map.js
+++ src/map/map.js
@@ -3,134 +3,143 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiMjVwYXNzZXIiLCJhIjoiY2t4MHNtNGh0MTB3dDJubHlrc
 const long = -90.5209854;
 const lat = -37.0214519;
 
+const pointDraw = (lng, lat) => {
 
-        const map = new mapboxgl.Map({
-            container: 'map', // container ID
-            style: 'mapbox://styles/mapbox/streets-v11', // style URL
-            center: [long, lat], // starting position [lng, lat]
-            zoom: 18 ,  // starting zoom
-            hash: true
-        });
+  let y = Math.random(999) * 10
 
-        
+  map.addSource(`points${[y]}`, {
+    'type': 'geojson',
+    'data': {
+      'type': 'FeatureCollection',
+      'features': [
+        {
+          'type': 'Feature',
+          'properties': {},
+          'geometry': {
+            'type': 'Point',
+            'coordinates': [lng, lat]
+          }
+        }
+      ]
+    }
+  });
 
-        map.on('load', () => {
-            // Add a GeoJSON source with 3 points.
-            // map.addSource('points', {
-            // 'type': 'geojson',
-            // 'data': {
-            // 'type': 'FeatureCollection',
-            // 'features': [
-            // {
-            // 'type': 'Feature',
-            // 'properties': {},
-            // 'geometry': {
-            // 'type': 'Point',
-            // 'coordinates': [long, lat]
-            // }
-            // },
-            // {
-            // 'type': 'Feature',
-            // 'properties': {},
-            // 'geometry': {
-            // 'type': 'Point',
-            // 'coordinates': [-90.3295, -0.6344]
-            // }
-            // },
-            // {
-            // 'type': 'Feature',
-            // 'properties': {},
-            // 'geometry': {
-            // 'type': 'Point',
-            // 'coordinates': [-91.3403, 0.0164]
-            // }
-            // }
-            // ]
-            // }
-            // });
+  map.addLayer({
+    'id': `circle${[y]}`,
+    'type': 'circle',
+    'source': `points${[y]}`,
+    'paint': {
+      'circle-color': '#4264fb',
+      'circle-radius': 8,
+      'circle-stroke-width': 2,
+      'circle-stroke-color': '#ffffff'
+    }
+  });
+  console.log(y);
+
+}
 
 
-            // // Add a circle layer
-            // map.addLayer({
-            // 'id': 'circle',
-            // 'type': 'circle',
-            // 'source': 'points',
-            // 'paint': {
-            // 'circle-color': '#4264fb',
-            // 'circle-radius': 8,
-            // 'circle-stroke-width': 2,
-            // 'circle-stroke-color': '#ffffff'
-            // }
-            // });
-             
-            // Center the map on the coordinates of any clicked circle from the 'circle' layer.
-            map.on('click', 'circle', (e) => {
-            map.flyTo({
-            center:  e.features[0].geometry.coordinates,
-            zoom: 15
-            });
-            
-            });
-             
-            // Change the cursor to a pointer when the it enters a feature in the 'circle' layer.
-            map.on('mouseenter', 'circle', () => {
-            map.getCanvas().style.cursor = 'pointer';
-            });
-             
-            // Change it back to a pointer when it leaves.
-            map.on('mouseleave', 'circle', () => {
-            map.getCanvas().style.cursor = '';
-            });
-            });
+const map = new mapboxgl.Map({
+  container: 'map', // container ID
+  style: 'mapbox://styles/mapbox/streets-v11', // style URL
+  center: [long, lat], // starting position [lng, lat]
+  zoom: 18,  // starting zoom
+  hash: true
+});
 
-            map.on('click', 'states-layer', (e) => {
-                new mapboxgl.Popup()
-                .setLngLat(e.lngLat)
-                .setHTML(e.features[0].properties.name)
-                .addTo(map);
-                });
 
-                map.on('style.load', function() {
-                    map.on('click', function(e) {
-                      var coordinates = e.lngLat; 
-                      new mapboxgl.Popup()
-                        .setLngLat(coordinates)
-                        .setHTML('Hiciste click en: <br/>' 
-                                  +'<b> Latitud: </b>'+ coordinates.lat + '<br/>'
-                                  +'<b> Longitud: </b>'+ coordinates.lng)
-                        .addTo(map);
 
-                        map.addSource('points', {
-                          'type': 'geojson',
-                          'data': {
-                          'type': 'FeatureCollection',
-                          'features': [
-                          {
-                          'type': 'Feature',
-                          'properties': {},
-                          'geometry': {
-                          'type': 'Point',
-                          'coordinates': [coordinates.lng, coordinates.lat]
-                          }
-                          }
-                          ]
-                          }
-                          });
-                          
-                          
-                      // Add a circle layer
-                      
-                      map.addLayer({
-                        'id': 'circle',
-                        'type': 'circle',
-                        'source': 'points',
-                        'paint': {
-                        'circle-color': '#4264fb',
-                        'circle-radius': 8,
-                        'circle-stroke-width': 2,
-                        'circle-stroke-color': '#ffffff'
-                        }
-                        });
+map.on('load', () => {
+  // Add a GeoJSON source with 3 points.
+  // map.addSource('points', {
+  // 'type': 'geojson',
+  // 'data': {
+  // 'type': 'FeatureCollection',
+  // 'features': [
+  // {
+  // 'type': 'Feature',
+  // 'properties': {},
+  // 'geometry': {
+  // 'type': 'Point',
+  // 'coordinates': [long, lat]
+  // }
+  // },
+  // {
+  // 'type': 'Feature',
+  // 'properties': {},
+  // 'geometry': {
+  // 'type': 'Point',
+  // 'coordinates': [-90.3295, -0.6344]
+  // }
+  // },
+  // {
+  // 'type': 'Feature',
+  // 'properties': {},
+  // 'geometry': {
+  // 'type': 'Point',
+  // 'coordinates': [-91.3403, 0.0164]
+  // }
+  // }
+  // ]
+  // }
+  // });
 
-                    });
-                  });
+
+  // // Add a circle layer
+  // map.addLayer({
+  // 'id': 'circle',
+  // 'type': 'circle',
+  // 'source': 'points',
+  // 'paint': {
+  // 'circle-color': '#4264fb',
+  // 'circle-radius': 8,
+  // 'circle-stroke-width': 2,
+  // 'circle-stroke-color': '#ffffff'
+  // }
+  // });
+
+  // Center the map on the coordinates of any clicked circle from the 'circle' layer.
+  map.on('click', 'circle', (e) => {
+    map.flyTo({
+      center: e.features[0].geometry.coordinates,
+      zoom: 15
+    });
+
+  });
+
+  // Change the cursor to a pointer when the it enters a feature in the 'circle' layer.
+  map.on('mouseenter', 'circle', () => {
+    map.getCanvas().style.cursor = 'pointer';
+  });
+
+  // Change it back to a pointer when it leaves.
+  map.on('mouseleave', 'circle', () => {
+    map.getCanvas().style.cursor = '';
+  });
+});
+
+map.on('click', 'states-layer', (e) => {
+  new mapboxgl.Popup()
+    .setLngLat(e.lngLat)
+    .setHTML(e.features[0].properties.name)
+    .addTo(map);
+});
+
+map.on('style.load', function () {
+  map.on('click', function (e) {
+    var coordinates = e.lngLat;
+    new mapboxgl.Popup()
+      .setLngLat(coordinates)
+      .setHTML('Hiciste click en: <br/>'
+        + '<b> Latitud: </b>' + coordinates.lat + '<br/>'
+        + '<b> Longitud: </b>' + coordinates.lng)
+      .addTo(map);
+
+    // Add a circle layer
+    pointDraw(coordinates.lng, coordinates.lat);
+  });
+});
+
+
+
